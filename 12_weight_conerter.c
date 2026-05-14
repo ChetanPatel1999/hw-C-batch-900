045e14a9f88aefbde280a0a7d33d4f8bc6271d4d
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-05-14 10:20:31 +0530

    11/05

--- 12_weight_conerter.c
+++ 12_weight_conerter.c
@@ -4,7 +4,7 @@ void main()
 {
     float kg, gram;
     printf("enter weight in kg : ");
-    scanf("%f", &kg); // 1.5
+    scanf("%f", &kg);
     gram = kg * 1000;
     printf("weight in gram = %.0f gram", gram);
 }
---
+++ 19_idenifier.c
@@ -0,0 +1,11 @@
+// example of identifire and constant
+# include <stdio.h>
+void main ( )
+{
+    int age = 50 ;
+    age = 100 ;
+
+    const float pi = 3.141;
+
+    // pi = 5.67;   we can not change value of constant variable
+}
