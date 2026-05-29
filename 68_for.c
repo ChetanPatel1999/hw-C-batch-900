d00870c327083a41f71bf1ec9c2929fcbca825a5
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-05-29 10:24:58 +0530

    25-05

---
+++ 68_for.c
@@ -0,0 +1,15 @@
+//  Write a program to display table of given number.
+// 4 * 1= 4
+// 4 * 2= 8
+// 4 * 3= 12
+#include <stdio.h>
+void main()
+{
+    int i, n;
+    printf("enter a num : ");
+    scanf("%d", &n);
+    for (i = 1; i <= 10; i++) // 4
+    {
+        printf("%d x %d = %d \n", n, i, n * i);
+    }
+}
