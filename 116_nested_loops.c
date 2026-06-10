970d740ca7a6b2d312788684dd2f0f722f4c5177
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-10 10:26:50 +0530

    08/06

---
+++ 116_nested_loops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++) // 2
+    {
+
+        for (j = 1; j <= 5; j++) // 2
+        {
+            printf("hello students \n");
+        }
+
+        printf("-------------------\n");
+    }
+}
